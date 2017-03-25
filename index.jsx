dad63e5293ed933ffbc9fbfdf029ba850b2eebf0
Jose Maria Toscano <tuertilindo@gmail.com>
2017-03-25 19:58:41 -0300

    adding export index

---
+++ index.jsx
@@ -0,0 +1,18 @@
+import Loading from './controls/Loading.jsx'
+import Boton from './controls/Boton.jsx'
+import Medalla from './controls/Medalla.jsx'
+import Hueso from './controls/Hueso.jsx'
+import Coins from './controls/Coins.jsx'
+import Pets from './controls/Pets.jsx'
+import Jumbotron from './controls/Jumbotron.jsx'
+
+let Controls = {
+  Loading: Loading,
+  Boton: Boton,
+  Medalla: Medalla,
+  Hueso: Hueso,
+  Coins: Coins,
+  Pets: Pets,
+  Jumbotron: Jumbotron
+}
+export default Controls
